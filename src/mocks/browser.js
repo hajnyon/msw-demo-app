962f47328c9619efd7ff7d212d451d341da86bff
Ondřej Hajný <ondrej@cookielab.io>
2024-11-13 18:07:03 +0100

    chore: updated msw

--- src/mocks/browser.js
+++ src/mocks/browser.js
@@ -12,9 +12,8 @@ export const worker = setupWorker(
 	// REST API mocking
 	http.get("/api/v1/data", () => HttpResponse.json(mockData)),
 	// WS mocking
-	wsLink.on("connection", ({ client }) => {
+	wsLink.addEventListener("connection", ({ client }) => {
 		client.addEventListener("message", (event) => {
-			console.log("🚀 ~ client.addEventListener ~ event:", event);
 			const data = JSON.parse(event.data);
 			switch (data.action) {
 				case "INCREMENT": {
